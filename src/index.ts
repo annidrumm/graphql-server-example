0151b59c381f8c0feccd0a8f5344cba2e425fab9
anni <ann-kristin.drumm@hausgold.de>
2022-12-12 15:30:54 +0100

    Update index

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,52 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
-const typeDefs = `
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = `#graphql
+# Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
+# This "Book" type defines the queryable fields for every book in our data source.
 type Book {
   title: String
   author: Author
 }
 
 type Author {
-  name: String
-  books: [Book]
+  name: String! # ! can't return null
+  books: [Book] # This list can't be null AND its list *items* can't be null
 }
-
-  type Query {
-    books: [Book]
-    authors: [Author]
-  }
+# The "Query" type is special: it lists all of the available queries that
+# clients can execute, along with the return type for each. In this
+# case, the "books" query returns an array of zero or more Books (defined above).
+type Query {
+  books: [Book]
+  authors: [Author]
+}
 `;
 
 const books = [
   {
+    id: 1,
     title: 'The Awakening',
-    author: 'Kate Chopin',
+    author: 1,
   },
   {
+    id: 2,
     title: 'City of Glass',
-    author: 'Paul Auster',
+    author: 2,
   },
 ];
 
 const authors = [
   {
+    id: 1,
     name: 'Kate Chopin',
+  },
+  {
+    id: 2,
+    name: 'Paul Auster',
   },
 ];
 
